9e89db224c05972169c4a7b5283c11169eca3e8a
Lukas Reitshammer <k11926540@students.jku.at>
2025-05-31 18:06:02 +0200

    Alle Aufgaben in ein Repo zusammenführen

--- src/main/java/se/jku/at/exercise/Main.java
+++ src/main/java/se/jku/at/exercise/Main.java
@@ -1,7 +1,46 @@
 package se.jku.at.exercise;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        class A01_TicTacToe {
+            public static void firstPart() {
+                //TODO: 1. Spielfeld-Ausgabe
+            }
+
+            public static void secondPart() {
+                //TODO 2. Fortgeschrittene Spielfeld-Ausgabe mit Variablen
+            }
+
+            public static void thirdPart() {
+                Scanner scanner = new Scanner(System.in);
+
+                //TODO: 3. Einlesen eines vollen Spielfeldstandes
+
+                scanner.close();
+            }
+        }
+
+        class A02_EinfachRechnungen {
+
+        }
+
+        class A03_Lohnberechnungen {
+
+        }
+
+        class A04_Betriebskostenrechner {
+
+        }
+
+        class A05_Angebotserstellung {
+
+        }
+
+        class A06_Wechselgeld_Berechnung {
+
+        }
 
     }
 }
